7fa5284f487049e0fb633538be128c99c85bff5d
tony nguyen <nguyen.viet.anh@cs.rooxim.com>
2024-03-22 15:54:42 +0700

    fix: add g-tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  // ssr: false,
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "nuxt-icon", "@nuxt/fonts"],
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "nuxt-icon", "@nuxt/fonts", "nuxt-gtag"],
   shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
     prefix: "",
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
     componentDir: "./components/ui",
   },
+  gtag: {
+    id: 'G-B4GGZE99P7'
+  }
 });
